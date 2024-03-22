27d61d5781b60b31a2c3914302e837a6b5fc9ea6
Ese Curtis <curtisese52@gmail.com>
2024-03-22 19:30:15 +0100

    feat: trying to fix

---
+++ test/user.js
@@ -0,0 +1,20 @@
+const user = {
+  id: '65fdcdd807466b71dafd2003',
+  name: 'Ese Curtis',
+  rank: 'D',
+  thoughts: ['Empathetic', 'Contemplative', 'Reserved', 'Introspective'],
+  abstracts: 'Age:20, Gender:Male, Race:Black or African American',
+  character: {
+    id: '65fdcdd807466b71dafd2004',
+    joy: 4,
+    anger: 2,
+    sadness: 1,
+    excitement: 2,
+    calmness: 1,
+    confidence: 0,
+    fear: 2,
+    kindness: 1,
+    honesty: 4,
+    userId: '65fdcdd807466b71dafd2003'
+  }
+}
